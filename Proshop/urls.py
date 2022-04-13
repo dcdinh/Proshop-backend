a4d5703c70a7687010b4fbd2d22ca9d90ed32b1f
dinh <congdinh.work@gmail.com>
2022-04-13 08:43:17 +0700

    developer

--- Proshop/urls.py
+++ Proshop/urls.py
@@ -31,3 +31,4 @@ urlpatterns = [
     re_path(r'^redoc/$', schema_view.with_ui('redoc',
             cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
